bd530f228de264723df6f8222d62c20c9e54203c
Andrea Abba (Office) <abba@nuclearinstruments.eu>
2019-07-01 20:00:17 +0200

    Initial release

---
+++ ispector_sdk.py
@@ -0,0 +1,92 @@
+import requests
+import json
+from enum import Enum
+class ispector_sdk:
+
+    class RunMode(Enum):
+        FREE =0
+        TIME_MS=1
+        COUNTS=2
+
+    class BaselineLength(Enum):
+        BASELINE_1024 =1024
+        BASELINE_512 =512
+        BASELINE_256 =256
+        BASELINE_128 = 256
+        BASELINE_64 = 64
+        BASELINE_32 = 32
+        BASELINE_16 = 32
+
+    API_ENDPOINT = ""
+
+    def __bool_to_str(self, bValue):
+        return  (str(bValue)).lower()
+
+    def __float_to_str(self, fValue):
+        return   str(fValue).replace(",",".")
+
+    def __int_to_str(self, iValue):
+        return   str(iValue)
+    def __init__(self, ip):
+        self.API_ENDPOINT = "http://" + ip
+
+    def set_hv_basic(self, hv_on, hv_voltage):
+        JSON_COMMAND = '{"command": "SET_CHANNEL_CONFIG", "channel_config": [{"id": 0, "HV_STATUS": ' + \
+                       self.__bool_to_str(hv_on)  + ', "HV_VOLTAGE": ' + \
+                       self.__float_to_str(hv_voltage) + '}], "store_flash": false}'
+        r = requests.post(url=(self.API_ENDPOINT + "/set_config.cgi"), data=JSON_COMMAND)
+
+    def set_hv_compensation(self, mode, temp_coeff):
+        JSON_COMMAND = '{"command": "SET_CHANNEL_CONFIG", "channel_config": [{"id": 0, "HV_MODE": ' + \
+                       mode + ', "TCoeff": ' + \
+                       self.__float_to_str(temp_coeff) + '}], "store_flash": false}'
+        r = requests.post(url=(self.API_ENDPOINT + "/set_config.cgi"), data=JSON_COMMAND)
+
+    def set_hv_cfg(self, ramp, maxI, maxV, on_starup):
+        JSON_COMMAND = '{"command" : "SET_CHANNEL_CONFIG","channel_config" :[{"id" : 0,"' \
+                       '"MaxV" : '+ self.__float_to_str(maxV) +',' \
+                       '"MaxI" : '+ self.__float_to_str(maxI)+',' \
+                       '"MaxT" : 0,' \
+                       '"RAMP" : '+ self.float_to_str(ramp) +',' \
+                       '"HV_PWRON" : '+ self.__bool_to_str(on_starup)  +'}],"store_flash" : false}'
+        r = requests.post(url=(self.API_ENDPOINT + "/set_config.cgi"), data=JSON_COMMAND)
+
+
+    def configureMCA(self, trigger_threshold,
+                     trigger_inibit, pre_int_time,
+                     int_time, int_gain, pileup_inib,
+                     pileup_penality, baseline_inib,
+                     baseline_len, target_run,
+                     target_value):
+
+
+        JSON_COMMAND = '{"command" : "SET_CHANNEL_CONFIG","mca_config" :[{"id" : 0,' \
+                       '"trigger_thrs" : '+ self.__int_to_str(trigger_threshold) +',' \
+                       '"trigger_inib" : '+ self.__int_to_str(trigger_inibit) +',' \
+                       '"int_pre" : '+ self.__int_to_str(pre_int_time) +',' \
+                       '"int_val" : '+ self.__int_to_str(int_time) +',' \
+                       '"int_gain" : '+ self.__int_to_str(int_gain) +',' \
+                       '"pileup_inib" : '+ self.__int_to_str(pileup_inib) +',' \
+                       '"pileup_pen" : '+ self.__int_to_str(pileup_penality) +',' \
+                       '"baseline_inib" : '+ self.__int_to_str(baseline_inib) +',' \
+                       '"baseline_len" : '+ self.__int_to_str(baseline_len.value) +',' \
+                       '"taget_run" : '+ self.__int_to_str(target_run.value) +',' \
+                       '"taget_value" : '+ self.__int_to_str(target_value)+',' \
+                       '"reset_on_apply" : true}],"store_flash" : false}}'
+        r = requests.post(url=(self.API_ENDPOINT + "/set_config.cgi"), data=JSON_COMMAND)
+
+    def getChannelStatus(self):
+        r = requests.get(url=(self.API_ENDPOINT + "/status.cgi"))
+        return json.loads(r.text)["current_status"]["channels"][0]
+
+    def getSystemStatus(self):
+        r = requests.get(url=(self.API_ENDPOINT + "/status.cgi"))
+        return json.loads(r.text)["current_status"]["system_status"]
+
+    def getWave(self):
+        r = requests.get(url=(self.API_ENDPOINT + "/wavedump.cgi"))
+        return json.loads(r.text)["data"]
+
+    def getSpectrum(self):
+        r = requests.get(url=(self.API_ENDPOINT + "/spectrum.cgi"))
+        return json.loads(r.text)["data"]
---
+++ test_ispector_sdk.py
@@ -0,0 +1,49 @@
+from ispector_sdk import ispector_sdk
+import pprint
+import numpy as np
+import matplotlib.pyplot as plt
+
+I1 = ispector_sdk("192.168.50.2")
+
+I1.set_hv_basic(True,41.5)
+ChStatus = I1.getChannelStatus()
+
+I1.configureMCA(trigger_threshold=100,
+                trigger_inibit=300,
+                pre_int_time=150,
+                int_time=10,
+                int_gain=100,
+                pileup_inib=25,
+                pileup_penality=25,
+                baseline_inib=25,
+                baseline_len=ispector_sdk.BaselineLength.BASELINE_512,
+                target_run=ispector_sdk.RunMode.FREE,
+                target_value=0
+                )
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(ChStatus)
+
+
+SysStatus = I1.getSystemStatus()
+
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(SysStatus)
+
+
+WaveMatrix = I1.getWave()
+
+A = np.array(WaveMatrix)
+wave_track = A[:,0]
+
+plt.plot(wave_track)
+plt.show()
+
+while 1:
+    SpectrumData = I1.getSpectrum()
+    plt.plot(SpectrumData)
+    plt.pause(1)
+
+plt.show()
+
+
